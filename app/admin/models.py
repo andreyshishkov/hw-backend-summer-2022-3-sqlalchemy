f6d21fa4632632c050aeea153943eb574d2b667f
Andrey Shishkov <shishkoffandrey@yandex.ru>
2026-02-23 16:14:04 +0300

    add fixes to admin model

--- app/admin/models.py
+++ app/admin/models.py
@@ -9,8 +9,8 @@ class AdminModel(BaseModel):
     __tablename__ = "admins"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    email: Mapped[str] = mapped_column(String(120), index=True, unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(256), nullable=False)
+    email: Mapped[str] = mapped_column(String, index=True, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
 
     def is_password_valid(self, password: str) -> bool:
         return self.password == hash_password(password)
