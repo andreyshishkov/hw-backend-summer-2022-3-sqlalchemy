4f33cc32ca4b71cdf4d0d320016f1957da687939
Andrey Shishkov <shishkoffandrey@yandex.ru>
2026-03-24 12:47:37 +0300

    fix app fug in alembic-env

--- alembic/env.py
+++ alembic/env.py
@@ -24,12 +24,13 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 from app.store.database import BaseModel
 from app.web.config import setup_config
+from app.web.app import app
 
 app_config_path = Path(__file__).parent.parent / 'config.yml'
-app_config = setup_config(app_config_path)
+setup_config(app, app_config_path)
 from app.admin.models import AdminModel
 from app.quiz.models import ThemeModel, QuestionModel, AnswerModel
-config.set_main_option('sqlalchemy.url', app_config.database.url)
+config.set_main_option('sqlalchemy.url', app.database.url)
 target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
